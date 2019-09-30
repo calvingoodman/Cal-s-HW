37db147d58d477c90539ed76f10bf6729dd04480
calvingoodman <54328252+calvingoodman@users.noreply.github.com>
2019-09-30 14:02:40 -0400

    counter created

---
+++ counters.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Calvin Goodman
+   Date Due:   9/25/2019
+ ******************************************************************************/
+import java.util.Scanner;
+
+public class counters
+{
+    public static void main(String[] args)
+    {
+        for (int i=2;i <=10; i+=2)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
