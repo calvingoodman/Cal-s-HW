b3b72f94b61a49e1bde4037b4bdc4349223e1cb8
calvingoodman <54328252+calvingoodman@users.noreply.github.com>
2019-10-10 20:00:26 -0400

    Update spelling.java
    -added space (need to optimize)
    -need random (see Madgab)
    -need loop (may need to move declaration of some variable/arrays)
    -need to ADD FLARE!! ;)

--- spelling.java
+++ spelling.java
@@ -19,6 +19,8 @@ public static void main(String[] args)
         String [] definitions;
         int number;
 
+        // Setting up word list w/ definitions
+
         System.out.println("How many spelling words do you have?");
                 number = KB.nextInt();
 
@@ -34,15 +36,102 @@ public static void main(String[] args)
 
         for (int i=1; i <= number; i++)
         {
-            System.out.printf("Now, enter the definition for word number " + i + ": ");
-                definitions [i] = KB.next() + KB.nextLine(); /*This solved the problem*/
+            System.out.printf("Now, enter the definition of " + spellingWords [i] + ": ");
+                definitions [i] = KB.next() + KB.nextLine(); //This solved the problem
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        // Preliminaries out of the way!
+        // Actual 'game' starts now
+        String answer;
+
+
+        System.out.println("Alright, you're all set!");
+
+
+        for (int i=1;i<=number;i++)
+        {
+        System.out.println("What is: " + definitions [i] + "?");
+            answer = KB.next();
+            if (answer.equals(spellingWords [i]))
+            {
+                System.out.println("Well done!");
+            }
+            else 
+            {
+                System.out.println("Incorrect");
+                System.out.println("The correct way to spell it is: " + spellingWords [i]);
+            }
+
+
         }
         
-
-
-
-        System.out.println(spellingWords [1]);
-        System.out.println(definitions [2]);
         
 
             
