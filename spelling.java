2ab8026defe9d94f84a0a008e04a1991d0990d34
calvingoodman <54328252+calvingoodman@users.noreply.github.com>
2019-10-11 23:15:36 -0400

    Update spelling.java
    -added screen cls code
    -the catch is running, instead of the try

--- spelling.java
+++ spelling.java
@@ -7,7 +7,7 @@
 // Spelling Review Helper - user gives spelling words w/ definitions. 
 //  Definitions given back randomly - user must then spell word correctly.
 import java.util.Scanner;
-
+import java.io.*;
 
 public class spelling
 {
@@ -16,7 +16,8 @@ public static void main(String[] args)
 
     {
         Scanner KB = new Scanner(System.in);
-        
+
+        Process  exitCode;
         int number; 
         String [] spellingWords;
         String [] definitions;
@@ -43,54 +44,29 @@ public static void main(String[] args)
             System.out.printf("Now, enter the definition of " + spellingWords [i] + ": ");
                 definitions [i] = KB.next() + KB.nextLine(); //This solved the problem
         }
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-    
-        public static void clearScreen() {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-        }
         
+//Attempting to clear screen
+        try {
+
+        if (System.getProperty("os.name").startsWith( "Window" ) ) {
+            exitCode = Runtime.getRuntime().exec( "cls" );
+        } else {
+        exitCode = Runtime.getRuntime().exec( "clear" );
+        }
+
+        } catch (java.io.IOException e) {
+            for (int i = 0; i < 1000; i++)  {
+                System.out.println();
+            }
+        }
+
+// It's not working!!! ;(
+    // It is trying, but the catch is running instead, giving 1000 empty print lines
+
+
+
+
+
         // Preliminaries out of the way!
         // Actual 'game' starts now
        System.out.println("Alright, you're all set!"); 
